d1cb0385ba893b61ae235af3bf4bd53aa88e60b6
tpanayiotis <99397970+tpanayiotis@users.noreply.github.com>
2023-04-02 22:34:11 +0100

    Add files via upload

---
+++ test/src/resources.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Author Endpoint
+ *@author Panagiotis Tamboukaris
+ */
+
+
+
+class Resources extends endpoint
+{
+
+    protected function initialiseSQL()
+    {
+        $sql = "SELECT DISTINCT * 
+                    FROM category";
+        $sqlParams = [];
+
+        if (filter_has_var(INPUT_GET, 'cat_id')) {
+            if (isset($where)) {
+                $where .= " AND cat_id = :cat_id";
+            } else {
+                $where = " WHERE cat_id = :cat_id";
+            }
+            $sqlParams['cat_id'] = $_GET['cat_id'];
+        }
+
+
+        if (isset($where)) {
+            $sql .= $where;
+        }
+
+        $this->setSQL($sql);
+        $this->setSQLParams($sqlParams);
+    }
+}
