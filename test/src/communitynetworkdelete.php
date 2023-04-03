98330e3daff6e472aa5b7cdd5c857a00a15c21d9
AlexantrosTamboutsiaris <99399073+AlexantrosTamboutsiaris@users.noreply.github.com>
2023-04-03 04:38:00 +0100

    Add files via upload

--- test/src/communitynetworkdelete.php
+++ test/src/communitynetworkdelete.php
@@ -1,47 +1,55 @@
 <?php
 
-class CommunityNetworkDelete extends api
+class CommunityNetworkDelete extends End
 {
     public function __construct()
     {
-        $this->validateRequestMethod("POST");
-        $this->validateDeleteParams();
         $db = new Database("db/tpp.db");
+
+        // Check the request method is POST
+        $this->validateRequestMethod("POST");
+
+        // Initialise and execute the SQL statement 
         $this->initialiseSQL();
         $queryResult = $db->executeSQL($this->getSQL(), $this->getSQLParams());
-        // No need to set status code
-        // Return a success message
+
         $this->setData(array(
             "length" => 0,
-            "message" => "Success",
+            "message" => "Success. Text deleted successfully.",
             "data" => null
         ));
-    }
-    private function validateRequestMethod($method)
-    {
-        if ($_SERVER['REQUEST_METHOD'] != $method) {
-            throw new ClientError("Invalid Request Method", 405);
-        }
-    }
-
-    private function validateDeleteParams()
-    {
-        if (!filter_has_var(INPUT_POST, 'main_id')) {
-            throw new ClientError("ID parameter required", 400);
-        }
-        // Add any other validation for required parameters
     }
 
     protected function initialiseSQL()
     {
-        $main_id = $_POST['main_id'];
-
         $sql = "DELETE FROM main
-                WHERE main_id = :main_id";
+                WHERE 'true' = 'true'";
 
-        $this->setSQL($sql);
-        $this->setSQLParams([
-            'main_id' => $main_id
-        ]);
+        $sqlParams = array();
+
+        $bool = true;
+
+        // Main ID parameter
+        if (filter_has_var(INPUT_GET, 'main_id')) {
+            $sql .= " AND main_id = :main_id";
+            $sqlParams[':main_id'] = $_GET['main_id'];
+
+            $param = 'main_id';
+            $name = "Main ID";
+
+            // Function that checks the correctness of the Category Main ID
+            SwitchStatementForIntegers($param, $name);
+        }
+
+        // if the parameter that the user inserts is OK, then set the SQL and the parameters
+        if ($bool) {
+            $this->setSQL($sql);
+            $this->setSQLParams($sqlParams);
+        }
+    }
+
+    protected function endpointParams()
+    {
+        return ['main_id'];
     }
 }
