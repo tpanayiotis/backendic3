e6c407abd79e47e828f0a67602d8b6e8c0e229df
AlexantrosTamboutsiaris <99399073+AlexantrosTamboutsiaris@users.noreply.github.com>
2023-04-03 04:30:51 +0100

    Add files via upload

--- test/src/categorytextdelete.php
+++ test/src/categorytextdelete.php
@@ -1,47 +1,55 @@
 <?php
 
-class CategoryTextDelete extends api
+class CategoryTextDelete extends End
 {
     public function __construct()
     {
-        $this->validateRequestMethod("POST");
-        $this->validateDeleteParams();
         $db = new Database("db/tpp.db");
+
+        // Check the request method is POST
+        $this->validateRequestMethod("POST");
+
+        // Initialise and execute the SQL statement 
         $this->initialiseSQL();
         $queryResult = $db->executeSQL($this->getSQL(), $this->getSQLParams());
-        // No need to set status code
-        // Return a success message
+
         $this->setData(array(
             "length" => 0,
-            "message" => "Success",
+            "message" => "Success. Text deleted successfully.",
             "data" => null
         ));
-    }
-    private function validateRequestMethod($method)
-    {
-        if ($_SERVER['REQUEST_METHOD'] != $method) {
-            throw new ClientError("Invalid Request Method", 405);
-        }
-    }
-
-    private function validateDeleteParams()
-    {
-        if (!filter_has_var(INPUT_POST, 'cat_text_id')) {
-            throw new ClientError("ID parameter required", 400);
-        }
-        // Add any other validation for required parameters
     }
 
     protected function initialiseSQL()
     {
-        $cat_text_id = $_POST['cat_text_id'];
-
         $sql = "DELETE FROM category_text
-                WHERE cat_text_id = :cat_text_id";
+                WHERE 'true' = 'true'";
 
-        $this->setSQL($sql);
-        $this->setSQLParams([
-            'cat_text_id' => $cat_text_id
-        ]);
+        $sqlParams = array();
+
+        $bool = true;
+
+        // Category Text ID parameter
+        if (filter_has_var(INPUT_GET, 'cat_text_id')) {
+            $sql .= " AND cat_text_id = :cat_text_id";
+            $sqlParams[':cat_text_id'] = $_GET['cat_text_id'];
+
+            $param = 'cat_text_id';
+            $name = "Category Text ID";
+
+            // Function that checks the correctness of the Category Text ID
+            SwitchStatementForIntegers($param, $name);
+        }
+
+        // if the parameter that the user inserts is OK, then set the SQL and the parameters
+        if ($bool) {
+            $this->setSQL($sql);
+            $this->setSQLParams($sqlParams);
+        }
+    }
+
+    protected function endpointParams()
+    {
+        return ['cat_text_id'];
     }
 }
